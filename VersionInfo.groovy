ad6e404cc33f8a5677b8a9512f78e4dbbd5cdd11
Michael Easter <codetojoy@gmail.com>
2021-11-02 09:23:14 -0300

    link fix

--- VersionInfo.groovy
+++ VersionInfo.groovy
@@ -7,7 +7,7 @@ def workingFile = new File("./tmp.info.html")
 def pattern = "dd-MM-yyyy HH:mm";
 def formatter = new SimpleDateFormat(pattern)
 def timestamp = formatter.format(new Date())
-def version = "v 0.9.1"
+def version = "v 0.9.2"
 
 def VERSION_TOKEN = '__HAIKU_VERSION'
 def TIMESTAMP_TOKEN = '__HAIKU_TIMESTAMP'
